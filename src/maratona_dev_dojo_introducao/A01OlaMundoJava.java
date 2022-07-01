a0f757668c6955fdd065e7b356acf788bd46e652
Isac Heres Lopes <isachlopes2@hotmail.com>
2022-07-01 20:50:12 -0300

    day02

---
+++ src/maratona_dev_dojo_introducao/A01OlaMundoJava.java
@@ -0,0 +1,15 @@
+package maratona_dev_dojo_introducao;
+
+/**
+ * isso é um comentario doido
+ */
+public class A01OlaMundoJava {
+    public static void main(String [] args){
+        System.out.println("KA-ME-HA-ME-HAAA!!!");
+    }
+}
+//isso e um comentario
+/*fjdifjdijf
+fdjfidjfijd
+foijdifjidj
+ */
---
+++ src/maratona_dev_dojo_introducao/A02TiposPrimitivos.java
@@ -0,0 +1,9 @@
+package maratona_dev_dojo_introducao;
+
+public class A02TiposPrimitivos {
+    public static void main(String[] args) {
+
+        int fatherAge = 24;
+        System.out.println("Sua idade é "+fatherAge+" anos");
+    }
+}
