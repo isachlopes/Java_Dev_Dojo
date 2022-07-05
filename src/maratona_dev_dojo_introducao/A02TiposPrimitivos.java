f5577b605f00b1746beef3279fb15db8c0bbe66b
Isac Heres Lopes <isachlopes2@hotmail.com>
2022-07-05 12:22:19 -0300

    to

--- src/maratona_dev_dojo_introducao/A02TiposPrimitivos.java
+++ src/maratona_dev_dojo_introducao/A02TiposPrimitivos.java
@@ -4,6 +4,22 @@ public class A02TiposPrimitivos {
     public static void main(String[] args) {
 
         int fatherAge = 24;
+        long numeroGrande = 10000;
+        double salarioDouble = 2000;
+        float salarioFloat = 3000.0F;
+        byte salarioByte = (byte) 234412;
+        short idadeShort = 30;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caracter = 'M';
+        String nome = "fomos morar no cu do mundo";
+
+
+
+
         System.out.println("Sua idade é "+fatherAge+" anos");
+        System.out.println(verdadeiro);
+        System.out.println(salarioByte);
+        System.out.println(nome);
     }
 }
